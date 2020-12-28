c39b431126f064d23218424f65727d06e9851f4f
CB <skive1000@hotmail.com>
2020-12-28 21:26:20 +0100

    Add project files.

---
+++ MyMath.Test.unit/MyMathTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+//using UnitTestAnotherDemo;
+
+namespace MyMath.Test.unit
+{
+    [TestFixture]
+    public class MyMathTest
+    {
+        [Test]
+        public void Add()
+        {
+            var math = new UnitTestAnotherDemo.MyMath();
+            var result = math.Add(20, 5);
+            Assert.AreEqual(25, result);
+        }
+
+        public void Add1()
+        {
+            var math = new UnitTestAnotherDemo.MyMath();
+            var result = math.Add1(64, 36);
+            Assert.That(result, Is.EqualTo(100));
+        }
+    }
+}
---
+++ UnitTestAnotherDemo/MyMath.cs
@@ -0,0 +1,15 @@
+﻿namespace UnitTestAnotherDemo
+{
+    public class MyMath
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Add1(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
